557985b29ce395315790af784a77bef3b377ae47
vidhi234 <sinhanidhi491@gmail.com>
2018-06-29 12:43:28 +0530

    android assingment

--- hlw_world/app/src/main/java/com/example/vidhisinha/myapplication/MainActivity.java
+++ hlw_world/app/src/main/java/com/example/vidhisinha/myapplication/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.vidhisinha.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void click(View v){
+      Toast.makeText(getApplicationContext(),"Button Clicked", Toast.LENGTH_SHORT).show();
     }
 }
