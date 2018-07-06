468e5d20a88b4de9af8dbe434a5b70c7c580a252
vidhi234 <sinhanidhi491@gmail.com>
2018-07-06 12:32:00 +0530

    android assingment

---
+++ class 2/toast/app/src/main/java/com/example/vidhisinha/toast/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.vidhisinha.toast;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toast.makeText(getApplicationContext(),"welcome to the my world",Toast.LENGTH_SHORT).show();
+    }
+}
---
+++ class 3/registorAPP/app/src/main/java/com/example/vidhisinha/registorapp/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.vidhisinha.registorapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import static android.icu.lang.UCharacter.GraphemeClusterBreak.V;
+import static android.widget.Toast.*;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText id;
+    private EditText password;
+
+    public Button button;
+    public void init() {
+        button = (Button) findViewById(R.id.button);
+        id =(EditText)findViewById(R.id.uid);
+        password =(EditText)findViewById(R.id.pass);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                validate(id.getText().toString(),password.getText().toString());
+            }
+        });
+    }
+    private  void validate(String usernmae,String password){
+        if ((usernmae.equals("vidhi")) && (password.equals("123"))){
+            Intent xyz=new Intent(MainActivity.this,view.class);
+            startActivity(xyz);
+        }
+        else makeText(getApplicationContext(), "Wrong Id && pass!!!!!!!!!!", LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        init();
+    }
+}
+
+
+
+
+
+
